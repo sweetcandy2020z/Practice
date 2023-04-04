c8a2185c09fb9cb41c2812a052af67177181b70b
Dinh <congdinhha99@gmail.com>
2023-04-04 10:45:10 +0700

    practice 1

---
+++ HCM23_FRF_EMB_01_PRACTICE_01/HCM23_FRF_EMB_01_PRACTICE_01.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = { -1, 0, 2 };
+
+    if (arr[0] < arr[1] && arr[0] < arr[2]) {
+        printf("Min: %d\n", arr[0]);
+    }
+    else if (arr[1] < arr[0] && arr[1] < arr[2]) {
+        printf("min: %d\n", arr[1]);
+    }
+    else
+        printf("min: %d\n", arr[2]);
+
+
+    if (arr[0] > arr[1] && arr[0] > arr[2]) {
+        printf("Max: %d\n", arr[0]);
+    }
+    else if (arr[1] > arr[0] && arr[1] > arr[2]) {
+        printf("Max: %d\n", arr[1]);
+    }
+    else
+        printf("Max: %d\n", arr[2]);
+    return 0;
+}
